c74c39da220bfff9b58cb7095b0a57f96136ca93
BharatJivvTate <hibharatj@gmail.com>
2026-04-13 10:37:31 +0530

    [ update ] - frontend admin and user profiles completed

--- frontend/Task-Manager/src/App.jsx
+++ frontend/Task-Manager/src/App.jsx
@@ -16,34 +16,50 @@ import MyTasks from './pages/User/MyTasks';
 import ViewTaskDetails from './pages/User/ViewTaskDetails';
 
 import PrivateRoute from './routes/PrivateRoute';
+import UserProvider from './context/userContext';
+import Root from './pages/Root';
+import { Toaster } from 'react-hot-toast';
 
 
 const App = () => {
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signUp" element={<SignUp />} />
+    <UserProvider>
+      <div>
+        <Router>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signUp" element={<SignUp />} />
 
-          {/* Admin Routes */}
-          <Route element={<PrivateRoute allowedRoles={["admin"]} />}>
-            <Route path="/admin/dashboard" element={<Dashboard />} />
-            <Route path="/admin/tasks" element={<ManageTasks />} />
-            <Route path="/admin/create-task" element={<CreateTask />} />
-            <Route path="/admin/users" element={<ManageUsers />} />
-          </Route>
+            {/* Admin Routes */}
+            <Route element={<PrivateRoute allowedRoles={["admin"]} />}>
+              <Route path="/admin/dashboard" element={<Dashboard />} />
+              <Route path="/admin/tasks" element={<ManageTasks />} />
+              <Route path="/admin/create-task" element={<CreateTask />} />
+              <Route path="/admin/users" element={<ManageUsers />} />
+            </Route>
 
-          {/* User Routes */}
-          <Route element={<PrivateRoute allowedRoles={["admin"]} />}>
-            <Route path="/user/dashboard" element={<UserDashboard />} />
-            <Route path="/user/tasks" element={<MyTasks />} />
-            <Route path="/user/tasks-details/:id" element={<ViewTaskDetails />} />
+            {/* User Routes */}
+            <Route element={<PrivateRoute allowedRoles={["admin"]} />}>
+              <Route path="/user/dashboard" element={<UserDashboard />} />
+              <Route path="/user/tasks" element={<MyTasks />} />
+              <Route path="/user/task-details/:id" element={<ViewTaskDetails />} />
+            </Route>
 
-          </Route>
-        </Routes>
-      </Router>
-    </div>
+            {/* Default Route */}
+            <Route path="/" element={<Root />} />
+          </Routes>
+        </Router>
+      </div>
+
+      <Toaster
+        toastOptions={{
+          className: "",
+          style: {
+            fontSize: "13px",
+          },
+        }}
+      />
+    </UserProvider>
   )
 }
 
---
+++ frontend/Task-Manager/src/pages/Root.jsx
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react'
+import { UserContext } from '../context/userContext'
+import { Navigate, Outlet } from 'react-router-dom'
+
+const Root = () => {
+    const { user, loading } = useContext(UserContext)
+    if(loading) return <Outlet />
+
+    if(!user) {
+        return <Navigate to="/login" />;
+    }
+    
+  return user.role === "admin" ? <Navigate to="/admin/dashboard" /> : <Navigate to="/user/dashboard" />
+}
+
+export default Root
